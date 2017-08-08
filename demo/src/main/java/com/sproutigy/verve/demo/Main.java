57b8e3efbf585e574a8208d90081ada97635c44f
LukeAheadNET <dev@lukeahead.net>
2017-08-08 07:30:15 +0200

    Demo Main class

---
+++ demo/src/main/java/com/sproutigy/verve/demo/Main.java
@@ -0,0 +1,9 @@
+package com.sproutigy.verve.demo;
+
+import com.sproutigy.verve.VerveQuickstart;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        VerveQuickstart.main(args);
+    }
+}
